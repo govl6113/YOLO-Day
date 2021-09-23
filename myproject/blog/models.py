550a3c32ed1a9a65dc26b854e3b977fda11f7219
{nmsgust} <nmsgust@gmail.com>
2021-09-24 00:36:56 +0900

    first

---
+++ myproject/blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}::{self.author}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()    
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
---
+++ myproject/blog/views.py
@@ -0,0 +1,97 @@
+
+from django.views.generic import ListView, DetailView,CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView
+from .models import Post, Comment
+from django.shortcuts import render, redirect
+from django.core.exceptions import PermissionDenied
+from .forms import CommentForm
+from django.shortcuts import get_object_or_404
+# Create your views here.
+
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+
+
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    comment.delete()
+    return redirect(post.get_absolute_url())
+
+
+
+
+def new_comment(request,pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+
+        if request.method =='POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post =post
+                comment.author =request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+
+#     return render(
+#         request, 'blog/index.html',{'posts':posts,}
+#     )
+class PostDetail(DetailView):
+    model = Post
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['comment_form']=CommentForm
+        return context
+# def single_post_page(request,pk):
+#     post = Post.objects.get(pk=pk)
+
+#     return render(request, 'blog/single_post_page.html', {'post':post,})
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self,form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author =current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model =Post
+    fields = ['title', 'content']
+
+    template_name = 'blog/post_update_form.html'
+
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+

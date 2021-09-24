9597514ce6d430306fb1ecd61c6e5d2a8211619d
{nmsgust} <nmsgust@gmail.com>
2021-09-25 00:34:23 +0900

    base.html 적용

--- myproject/single_pages/urls.py
+++ myproject/single_pages/urls.py
@@ -3,9 +3,9 @@ from . import views
 from . import views as single_pages
 
 urlpatterns = [
-    path('about_me/', views.about_me),
+    path('about_me/', views.about_me, name="about_me"),
     path('login/', single_pages.login, name="login"),
     path('logout/', single_pages.logout, name="logout"),
     path('login1/',views.login1,name="login1"),
-    
+
 ]

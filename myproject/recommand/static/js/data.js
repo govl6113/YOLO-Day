1bb4707245f811448d6c335cdd3056ac07332680
unknown <jjwzzy4643@gmail.com>
2021-09-24 23:06:44 +0900

    테스트 알고리즘데이터 ver1

---
+++ myproject/recommand/static/js/data.js
@@ -0,0 +1,93 @@
+const qnaList = [
+    {
+    q : '1. 나의 나이대는?',
+    a : [
+        {answer : 'a. 10대', type : []},
+        {answer : 'b. 20대', type : []},
+        {answer : 'c. 30대~40대', type : []},
+        {answer : 'd. 50대 이상', type : []},
+    ]
+},
+{
+    q : '2. 나의 성향은?',
+    a : [
+        {answer : 'a. 집순이', type : []},
+        {answer : 'b. 활동가', type : []},
+    ]
+},
+{
+    q : '3. 나는 보통?',
+    a : [
+        {answer : 'a. 걷는다', type : []},
+        {answer : 'b. 뛴다', type : []},
+        {answer : 'c. 뭔가를 탄다', type : []},
+    ]
+},
+{
+    q : '4. 루이스 폴센이 뭔지 안다',
+    a : [
+        {answer : 'a. 예', type : []},
+        {answer : 'b. 아니오', type : []},
+    ]
+},
+{
+    q : '5. 여행가면 맡는 역할?',
+    a : [
+        {answer : 'a. 숨 쉬기', type : []},
+        {answer : 'b. 사진 담당', type : []},
+        {answer : 'c. 정리 담당', type : []},
+        {answer : 'd. 텐션 담당', type : []},
+    ]
+},
+{
+    q : '6. 콘서트를 간다면',
+    a : [
+        {answer : 'a. 영상 찍기', type : []},
+        {answer : 'b. 눈으로 보기', type : []},
+    ]
+},
+{
+    q : '7. 핸드폰에 게임이 2개 이상 있다',
+    a : [
+        {answer : 'a. 예', type : []},
+        {answer : 'b. 아니오', type : []},
+    ]
+},
+{
+    q : '8. 지금 당장 집에 참기름이 어디있는지 안다',
+    a : [
+        {answer : 'a. 예', type : []},
+        {answer : 'b. 아니오', type : []},
+    ]
+},
+{
+    q : '9. 넥스트 레벨을 따라 춰 본 적이 있다',
+    a : [
+        {answer : 'a. 예', type : []},
+        {answer : 'b. 아니오', type : []},
+    ]
+},
+{
+    q : '10. 클래식을 들으면 솔직히 잔다',
+    a : [
+        {answer : 'a. 예', type : []},
+        {answer : 'c. 30대~40대', type : []},
+    ]
+},
+{
+    q : '11. 취미를 위한 지출 비용 얼마까지?',
+    a : [
+        {answer : 'a. 무료', type : []},
+        {answer : 'b. 5만원 이하', type : []},
+        {answer : 'c. 10만원 이하', type : []},
+        {answer : 'd. 10만원 초과', type : []},
+    ]
+},
+{
+    q : '12. 취미란',
+    a : [
+        {answer : 'a. 에너지를 얻기 위한 것', type : []},
+        {answer : 'b. 머리를 비우기 위한 것', type : []},
+    ]
+},
+]

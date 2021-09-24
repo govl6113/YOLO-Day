e43cb6f833a0fcc69871e15dde99cc758ad51cdf
Bae-jihyun <govl6113@naver.com>
2021-09-24 17:39:12 +0900

    메인 페이지 작업 중

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')), #구글 
     path('single_page/', include('single_pages.urls')),
     
-]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+]
